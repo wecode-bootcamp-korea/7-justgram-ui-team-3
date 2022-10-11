e527f2cb71c1697cc71c799bee1687951760f6a4
Jeong-Dagyeong <jdk09140@gmail.com>
2022-10-12 03:44:24 +0900

    mission4 완료

--- dagyeong/js/main.js
+++ dagyeong/js/main.js
@@ -1,29 +1,28 @@
 //comment 버튼클릭 이벤트
-const commentList = document.querySelector(".sentence"); // array로 들어옴
-const commentButtonList = document.querySelector(".feed-comment-input-button"); // array로 들어옴
+const commentList = document.getElementsByClassName("sentence"); // array로 들어옴
+const commentButtonList = document.getElementsByClassName(
+  "feed-comment-input-button"
+); // array로 들어옴
 
 console.log(commentList);
-console.log(commentButtonList);
 
 const commentArray = Array.from(commentList); // list를 array로 치환
-const commentButtonArray = Array.from(commentButtonList);
-
 console.log(commentArray);
-console.log(commentButtonArray);
+const commentButtonArray = Array.from(commentButtonList);
 
 commentButtonArray.forEach((button, index) => {
   button.addEventListener("click", () => {
-    const input = document.getElementsByClassName("feed-comment-input")[index];
-    const commentList = document.getElementsByClassName("sentence")[index];
+    const input = document.getElementsByClassName("feed-comment-input")[index]; // 0번째 버튼을 누르면 0번째의 comment-input을 등록
+    const commentList = document.getElementsByClassName("sentence")[index]; // list 요소(class="sentence")에 자식요소 child를 넣어준다.
 
     const commentCard = document.createElement("div");
-    commentCard.classList.add("sentence");
+    commentCard.classList.add("feed-comment-card");
 
-    const nickname = document.createElement("p");
+    const nickname = document.createElement("span");
     nickname.classList.add("highlight");
     nickname.textContent = "database55";
 
-    const contentSpan = document.createElement("p");
+    const contentSpan = document.createElement("span");
     contentSpan.textContent = input.value;
 
     commentCard.append(nickname, contentSpan);
